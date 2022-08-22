41626356264fe2227cc551187c9af1113888ca70
Sathiyanarayanan33 <111435779+Sathiyanarayanan33@users.noreply.github.com>
2022-08-22 14:45:28 +0530

    Add files via upload
    We use the extends in method

---
+++ Father.java
@@ -0,0 +1,19 @@
+class Father{
+  int a=2;
+  int b=5;
+}
+
+ class son extends Father{
+  public static void main(String ar []) 
+ {
+   son obj=new son();
+   obj.a=10;
+   System.out.println(obj.a);
+}
+
+
+}
+
+
+
+
